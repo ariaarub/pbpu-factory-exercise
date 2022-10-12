d41a6274dbcf46615e2410aa88b9e3949ac09c4f
ariaarub <95572966+ariaarub@users.noreply.github.com>
2022-10-12 12:10:00 +0700

    modified:   bin/App.class 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,11 +1,20 @@
 public class App {
+
     public static void main(String[] args) throws Exception {
         Autofactory fac = new Suzuki();
         Autofactory fac2 = new Honda();
 
-        fac.createBike();
-        fac.createCar();
-        fac2.createCar();
-        fac2.createBike();
+        Bike bike = fac.createBike();
+        Car car = fac.createCar();
+        Car car2 = fac2.createCar();
+        Bike bike2 = fac2.createBike();
+
+        System.out.println(bike.getClass().getName());
+        System.out.println(car.getClass().getName());
+        System.out.println(car2.getClass().getName());
+        System.out.println(bike2.getClass().getName());
+
+
+
     }
 }
